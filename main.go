20582fca353e3097ae2eb42bb5332c781daee148
Ramiz Polić <32913827+fhivemind@users.noreply.github.com>
2023-04-19 13:29:56 +0200

    add initial plant webhooks

--- main.go
+++ main.go
@@ -85,6 +85,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Plant")
 		os.Exit(1)
 	}
+	if err = (&operatorv1.Plant{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Plant")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
